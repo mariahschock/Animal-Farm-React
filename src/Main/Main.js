4421732166de204e1fe2bde6699e99a8615d1bd7
Mariah <mariahschock@gmail.com>
2022-06-07 12:25:57 -0700

    created folders/files, added pictures, worked on Header and Main

---
+++ src/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+import backgroundImage from '../background.png';
+
+export default function Main() {
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImage})` }}>
+        
+    </main>
+  );
+}
